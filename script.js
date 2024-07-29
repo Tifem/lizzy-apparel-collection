16ada87399210dbd61896d0c113572b81764f2df
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2024-07-28 18:38:56 -0700

    responsiveness fixing

---
+++ script.js
@@ -0,0 +1,23 @@
+// const closeIcon = document.querySelector('.close-menu');
+// const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+const openMenu = () =>{
+    menu.style.display = "grid";
+}
+
+const closeMenu = () =>{
+    menu.style.display = "none";
+
+}
+
+//method 2
+
+// menuIcon.addEventListener('click', () =>{
+//     openMenu()
+// })
+
+// closeIcon.addEventListener('click', () =>{
+//     closeMenu()
+// })
+
